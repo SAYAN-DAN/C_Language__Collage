2ab7374449289c5f1cd753e5db001fdc36c98320
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-06-09 07:58:45 +0530

    Solve  4 more Questions

---
+++ Sir/Question24.c
@@ -0,0 +1,15 @@
+// Write a program to to find the area of a circle using function
+#include<stdio.h>
+float findarea(int n){
+    return 3.14* n *n;
+}
+
+int main() {
+    int n;
+    printf("Enter the radius: ");
+    scanf("%d", &n);
+    float area = findarea(n);
+    printf("The area of a circle is: %f", area);
+    return 0;
+}
+
