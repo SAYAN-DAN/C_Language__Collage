542c87432da7d9d28ae3423d6cfcc6c01058f68f
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-07-29 09:20:37 +0530

    Commit

---
+++ Sir/Question41.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+int main() {
+    int  u = 3;
+    int v;
+    // int *pu;
+    // int *pv;
+    int *pu = &u;
+    v = *pu;
+    int *pv = &v;
+    printf("\n u = %d &u = %x pu = %x *pu = %d", u,&u,pu,*pu);    
+    printf("\n v = %d &v = %x pv = %x *pv = %d", v,&v,pv,*pv);    
+}
