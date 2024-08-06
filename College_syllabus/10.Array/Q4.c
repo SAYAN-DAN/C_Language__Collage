f77f108c7f475f20163e88477905247887eff628
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-08-06 07:16:13 +0530

    Commit

--- College_syllabus/10.Array/Q4.c
+++ College_syllabus/10.Array/Q4.c
@@ -1,31 +1,36 @@
 // Print the maximum and minimum element of array
 
 #include<stdio.h>
+
 int main() {
     int n, i;
     printf("Enter the number of elements in the array: ");
     scanf("%d", &n);
+    
     int arr[n];
-    printf("Enter the %d Element: ", n);
-    for (i = 0; i < n; i++)
-    {
+    printf("Enter the %d elements: ", n);
+    for (i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
     
     int maxvalue = arr[0];
     int minvalue = arr[0];
-    for ( i = 0; i < n-1; i++)
-    {
-        if (maxvalue < arr[i])
-        {
+    
+    for (i = 1; i < n; i++) {  // Start from i = 1 since arr[0] is already assigned to maxvalue and minvalue
+        if (maxvalue < arr[i]) {
             maxvalue = arr[i];
         }
-        if (minvalue > arr[i])
-        {
+        if (minvalue > arr[i]) {
             minvalue = arr[i];
         }       
     }
-    printf("The maximum value is:%d", maxvalue);
-    printf("The minvalue value is:%d", minvalue);
+    
+    printf("The maximum value is: %d\n", maxvalue);
+    printf("The minimum value is: %d\n", minvalue);
+    
     return 0;    
 }
+
+
+
+
--- practice.c
+++ practice.c
@@ -1,29 +1,45 @@
 
 
 
-#include<stdio.h>
-#include<conio.h>
-#include<string.h>
-#include<ctype.h>
 
-void main()
-{
-    int i, l;
-    char a[50];
-    clrscr();
-    printf("Enter a String:\n");
-    gets(a);
-    l = strlen(a);
-    for(i = 0; i < l; i++)
-    {
-        if(a[i] >= '0' && a[i] <= '9')
-            printf("0");
-        else if(a[i] >= 'A' && a[i] <= 'Z')
-            printf("%c", tolower(a[i]));
-        else if(a[i] >= 'a' && a[i] <= 'z') // Corrected this line
-            printf("%c", toupper(a[i]));
-        else
-            printf("*");
-    }
-    getch();
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include<stdio.h>
+// #include<conio.h>
+// #include<string.h>
+// #include<ctype.h>
+
+// void main()
+// {
+//     int i, l;
+//     char a[50];
+//     clrscr();
+//     printf("Enter a String:\n");
+//     gets(a);
+//     l = strlen(a);
+//     for(i = 0; i < l; i++)
+//     {
+//         if(a[i] >= '0' && a[i] <= '9')
+//             printf("0");
+//         else if(a[i] >= 'A' && a[i] <= 'Z')
+//             printf("%c", tolower(a[i]));
+//         else if(a[i] >= 'a' && a[i] <= 'z') // Corrected this line
+//             printf("%c", toupper(a[i]));
+//         else
+//             printf("*");
+//     }
+//     getch();
+// }
+
