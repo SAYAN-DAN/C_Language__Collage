09cfaea130711783066b9aaec24bc0c45a7d292a
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-08-12 21:48:26 +0530

    Commit

--- practice.c
+++ practice.c
@@ -1,7 +1,16 @@
 
 
+#include <stdio.h>
 
+int main() {
+    int a = 5;
+    int b = 8;
+    int c = 18;
 
+    printf("The equation of the line is: %dx + %dy = %d\n", a, b, c);
+
+    return 0;
+}
 
 
 // #include<stdio.h>
