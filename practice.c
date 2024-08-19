40a59da0332bd22394e9c56d93505392e45e2850
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-08-19 21:54:47 +0530

    Commit

--- practice.c
+++ practice.c
@@ -1,8 +1,21 @@
 
 #include<stdio.h>
+ void swap(int *a , int *b) {
+      int temp = *a;
+      *a = *b;
+      *b = temp;
+      return;
+ }
+
+
 int main() {
-    static char a[3][4] = {"abcd" , "mnop", "fghi"};
-    putchar(**a);
+    int num1,num2;
+    printf("Enter the numbers: ");
+    scanf("%d %d", &num1,&num2);
+    printf("Before the swapping the number is %d %d\n", num1,num2);
+    swap(&num1 , &num2);
+    printf("After the swapping the number is %d %d", num1,num2);
+    return 0;
 }
 // #include<stdio.h>
 // #include<conio.h>
