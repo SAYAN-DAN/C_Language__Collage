8406ac89d1a589954369a8f4cf035e852c4e0cd8
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-09-09 09:05:44 +0530

    Commit

--- practice.c
+++ practice.c
@@ -1,5 +1,16 @@
 
-
+#include<stdio.h>
+int main() {
+    int n, fact=1;
+    printf("Enter the number: ");
+    scanf("%d" , &n);
+    for (int i = 1; i <=n; i++)
+    {
+        fact = fact*i;
+    }
+    printf("The factorial of number is: %d", fact);
+    return 0;    
+}
 
 
 // #include<stdio.h>
