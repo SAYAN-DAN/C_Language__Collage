9795c8da7b6e10e3cdefd3ac6ef04f1dcf715dac
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-09-11 11:00:15 +0530

    Commit

--- practice.c
+++ practice.c
@@ -1,28 +1,9 @@
-#include <stdio.h>
-int FindGCd(int a, int b);
-int main()
-{
-    int n1;
-    printf("Enter the number1: ");
-    scanf("%d", &n1);
-    int n2;
-    printf("Enter the number1: ");
-    scanf("%d", &n2);
-    int gcd = FindGCd(n1, n2);
-    printf("The GCD value of %d and %d is %d", n1, n2, gcd);
+#include<stdio.h>
+int main() {
+    int n;
+    printf("Enter the number: ");
+    scanf("%d", )
 }
-int FindGCd(int a, int b)
-{
-    if (b != 0)
-    {
-        return FindGCd(b, a % b);
-    }
-    else
-    {
-        return a;
-    }
-}
-
 // #include<stdio.h>
 // #include<conio.h>
 // #include<string.h>
