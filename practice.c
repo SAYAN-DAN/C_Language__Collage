7d5be861fedc16aa4fd66f0320b0177dfe81c36f
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-09-23 07:51:27 +0530

    An Another Approach in Conditional Statemen

--- practice.c
+++ practice.c
@@ -1,27 +1,120 @@
+#include <stdio.h>
 
-// #include<stdio.h>
-// #include<conio.h>
-// #include<string.h>
-// #include<ctype.h>
+int if_else_case()
+{
+    int a;
+    scanf("%d", &a);
+    if (a <= 9 && a >= 1)
+    {
+        if (a == 1)
+        {
+            printf("one");
+        }
+        else if (a == 1)
+        {
+            printf("one");
+        }
+        else if (a == 2)
+        {
+            printf("two");
+        }
+        else if (a == 3)
+        {
+            printf("three");
+        }
+        else if (a == 4)
+        {
+            printf("four");
+        }
+        else if (a == 5)
+        {
+            printf("five");
+        }
+        else if (a == 6)
+        {
+            printf("six");
+        }
+        else if (a == 7)
+        {
+            printf("seven");
+        }
+        else if (a == 8)
+        {
+            printf("eight");
+        }
+        else if (a == 9)
+        {
+            printf("nine");
+        }
+    }
+    else
+    {
+        printf("Greater than 9");
+    }
+    return 0;
+}
 
-// void main()
-// {
-//     int i, l;
-//     char a[50];
-//     clrscr();
-//     printf("Enter a String:\n");
-//     gets(a);
-//     l = strlen(a);
-//     for(i = 0; i < l; i++)
-//     {
-//         if(a[i] >= '0' && a[i] <= '9')
-//             printf("0");
-//         else if(a[i] >= 'A' && a[i] <= 'Z')
-//             printf("%c", tolower(a[i]));
-//         else if(a[i] >= 'a' && a[i] <= 'z') // Corrected this line
-//             printf("%c", toupper(a[i]));
-//         else
-//             printf("*");
-//     }
-//     getch();
-// }
+int switch_case()
+{
+    // Write Your Code Here
+    int number;
+    scanf("%d", &number);
+    switch (number)
+    {
+    case 1:
+        printf("one");
+        break;
+    case 2:
+        printf("two");
+        break;
+    case 3:
+        printf("three");
+        break;
+    case 4:
+        printf("four");
+        break;
+    case 5:
+        printf("five");
+        break;
+    case 6:
+        printf("six");
+        break;
+    case 7:
+        printf("seven");
+        break;
+    case 8:
+        printf("eight");
+        break;
+    case 9:
+        printf("nine");
+        break;
+    default:
+        printf("Greater than 9");
+    }
+    return 0;
+}
+
+int another_approach()
+{
+
+    int a;
+    const char *cases[] = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    scanf("%d", &a);
+
+    if (a >= 1 && a <= 9)
+    {
+        printf("%s", cases[a]);
+    }
+    else
+    {
+        printf("Greater than 9");
+    }
+    return 0;
+}
+int main()
+{
+    another_approach();
+    // if_else_case();
+    // switch_case();
+}
