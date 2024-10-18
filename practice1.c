2acfa3cef7aa3c175f02b5cfd418e599d1130a3e
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-10-18 08:24:29 +0530

    Commit

--- practice1.c
+++ practice1.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 // int main(){
 //     int n,i;
 //     printf("Enter a number: ");
@@ -6,13 +6,13 @@
 //     for ( i = 2; i <= n-1; i++){
 //         if (n % i ==0){
 //             printf("This is not a prime number");
-//             break;        
+//             break;
 //         }
 //     }
 //     if ( i == n){
 //         printf("This is a prime number");
 //     }
-//     return 0;    
+//     return 0;
 // }
 
 // int main() {
@@ -21,3 +21,12 @@
 //   int *ptr2 = ptr + 2;
 //   printf("%d", (ptr2-ptr));
 // }
+
+int main()
+{
+    // Null Pointer
+    int *ptr = NULL;
+    printf("The value of ptr is :%d\n", ptr);
+    printf("The value of ptr is :%d", *ptr);
+    return 0;
+}
