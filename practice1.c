232a515150f70664acfd168f60c5e0f2923800b5
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-10-18 07:11:14 +0530

    Learn one small concept

--- practice1.c
+++ practice1.c
@@ -1,16 +1,23 @@
 #include<stdio.h>
-int main(){
-    int n,i;
-    printf("Enter a number: ");
-    scanf("%d", &n);
-    for ( i = 2; i <= n-1; i++){
-        if (n % i ==0){
-            printf("This is not a prime number");
-            break;        
-        }
-    }
-    if ( i == n){
-        printf("This is a prime number");
-    }
-    return 0;    
-}
+// int main(){
+//     int n,i;
+//     printf("Enter a number: ");
+//     scanf("%d", &n);
+//     for ( i = 2; i <= n-1; i++){
+//         if (n % i ==0){
+//             printf("This is not a prime number");
+//             break;        
+//         }
+//     }
+//     if ( i == n){
+//         printf("This is a prime number");
+//     }
+//     return 0;    
+// }
+
+// int main() {
+//   int a=100;
+//   int *ptr = &a;
+//   int *ptr2 = ptr + 2;
+//   printf("%d", (ptr2-ptr));
+// }
