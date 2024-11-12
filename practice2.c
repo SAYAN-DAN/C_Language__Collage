92625de00109041f3a6759481b7f23d9d2330f0e
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-11-12 12:46:26 +0530

    Submit

--- practice2.c
+++ practice2.c
@@ -1,33 +1,14 @@
 #include <stdio.h>
-#include <string.h>
-
-void reverseString(char *str)
+int sum(int *a, int *b)
 {
-    int start = 0;
-    int end = strlen(str) - 1;
-    char temp;
-
-    // Swap characters from start and end until the middle is reached
-    while (start < end)
-    {
-        temp = str[start];
-        str[start] = str[end];
-        str[end] = temp;
-        start++;
-        end--;
-    }
+    int sum = *a + *b;
+    return sum;
 }
-
 int main()
 {
-    char str[100];
-
-    printf("Enter a string: ");
-    scanf("%s", str);
-
-    reverseString(str);
-
-    printf("Reversed string: %s\n", str);
-
+    int a = 10;
+    int b = 12;
+    int sumans = sum(&a, &b);
+    printf("%d", sumans);
     return 0;
 }
