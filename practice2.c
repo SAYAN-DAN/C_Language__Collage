a3c1efd069e11b3f1814cf9d070d56dbe052f28b
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-11-13 12:23:40 +0530

    Commit

--- practice2.c
+++ practice2.c
@@ -1,14 +1,8 @@
-#include <stdio.h>
-int sum(int *a, int *b)
-{
-    int sum = *a + *b;
-    return sum;
-}
-int main()
-{
-    int a = 10;
-    int b = 12;
-    int sumans = sum(&a, &b);
-    printf("%d", sumans);
-    return 0;
-}
+   #include <stdio.h>
+    int main()
+    {
+        int p ;
+        p = 4;
+        printf("p is %d", p);
+        return 0;
+    }
