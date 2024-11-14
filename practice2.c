b55e046e7c001fc628c2ac71dc3a5f1ea21bdd16
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-11-14 21:30:26 +0530

    submit

--- practice2.c
+++ practice2.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
+void foo();
 int main()
 {
-    int x = 0;
-    if (x++)
-        printf("true\n");
-    else if (x == 1)
-        printf("false\n");
+   void foo(int);
+   foo(1);
+   return 0;
+}
+void foo(int i)
+{
+   printf("2 ");
+}
+
+
+//--------------------------------------------
+
+#include <stdio.h>
+
+// Global declaration of foo()
+void foo(int);
+
+int main()
+{
+    foo(1);  // Call foo() with argument 1
+    return 0;
+}
+
+// Function definition of foo
+void foo(int i)
+{
+    printf("2 ");
 }
