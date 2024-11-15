b68bec300461a5fae6de753066e8a86c830a6c15
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-11-15 07:51:06 +0530

    Commit

--- practice2.c
+++ practice2.c
@@ -1,32 +1,13 @@
-#include <stdio.h>
-void foo();
-int main()
-{
-   void foo(int);
-   foo(1);
-   return 0;
-}
-void foo(int i)
-{
-   printf("2 ");
-}
-
-
-//--------------------------------------------
-
-#include <stdio.h>
-
-// Global declaration of foo()
-void foo(int);
-
-int main()
-{
-    foo(1);  // Call foo() with argument 1
-    return 0;
-}
-
-// Function definition of foo
-void foo(int i)
-{
-    printf("2 ");
-}
+    #include <stdio.h>
+    void m();
+    void n()
+    {
+        m();
+    }
+    void main()
+    {
+        void m()
+        {
+            printf("hi");
+        }
+    }
