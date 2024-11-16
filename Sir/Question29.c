01992aae17a50c987d32f994f38153dee2b95aa1
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-11-16 17:24:29 +0530

    Submit

--- Sir/Question29.c
+++ Sir/Question29.c
@@ -1,16 +1,18 @@
 // Write a program to add two number using function
-#include<stdio.h>
-int addTwo(int a,int b){
-    return a+b;
+#include <stdio.h>
+int addTwo(int a, int b)
+{
+    return a + b;
 }
 
-int main() {
-    int a,b,sum;
+int main()
+{
+    int a, b, sum;
     printf("Enter the number1: ");
     scanf("%d", &a);
     printf("Enter the number2: ");
-    scanf("%d" , &b);
-    sum = addTwo(a,b);
+    scanf("%d", &b);
+    sum = addTwo(a, b);
     printf("The sum of two number is: %d", sum);
     return 0;
 }
