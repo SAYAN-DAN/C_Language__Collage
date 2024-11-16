f9ea4cf8984c5a06fe2d45a271a494d62eb5ddd3
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-11-16 20:37:04 +0530

    Submit

--- Sir/Question38.c
+++ Sir/Question38.c
@@ -1,36 +1,37 @@
 // Write a program to find the largest from matrix...
 
-#include<stdio.h>
-int main() {
-    int arr[3][3], i,j,max;
-    for ( i = 0; i < 3; i++)
+#include <stdio.h>
+int main()
+{
+    int arr[3][3], i, j, max;
+    for (i = 0; i < 3; i++)
     {
-        for ( j = 0; j < 3; j++)
+        for (j = 0; j < 3; j++)
         {
-            printf("Enter arr[%d] [%d]:", i,j);
+            printf("Enter arr[%d] [%d]:", i, j);
             scanf("%d", &arr[i][j]);
-        }        
-    }
-    printf("\n The matrix is \n");
-    for ( i = 0; i < 3; i++)
-    {
-        printf("\n");
-        for ( j = 0; j < 3; j++)
-        {
-            printf("%d\t", arr[i][j]);
-        }        
-    }
-    max = arr[0][0];
-    for ( i = 0; i < 3; i++)
-    {
-        for ( j = 0; j < 3; j++)
-        {
-            if ( arr[i][j] > max)
-            {
-                max = arr[i][j];
-            }            
         }
     }
-    printf("The max term of x is %d", max);    
-    return 0;    
+    printf("\n The matrix is \n");
+    for (i = 0; i < 3; i++)
+    {
+        printf("\n");
+        for (j = 0; j < 3; j++)
+        {
+            printf("%d\t", arr[i][j]);
+        }
+    }
+    max = arr[0][0];
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (arr[i][j] > max)
+            {
+                max = arr[i][j];
+            }
+        }
+    }
+    printf("The max term of x is %d", max);
+    return 0;
 }
