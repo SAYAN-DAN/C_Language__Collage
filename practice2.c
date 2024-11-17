1b697d41fa36526b81e9f3cba759eb6798cddadf
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-11-17 09:21:53 +0530

    Submit

--- practice2.c
+++ practice2.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-
-int main()
+int x = 0;
+void main()
 {
-    int a = -1, b = 4, c = 1, d;
-
-    d = ++a && ++b || ++c;
-
-    printf("%d, %d, %d, %d\n", a, b, c, d);
-
-    return 0;
+    int *ptr = &x;
+    printf("%p\n", ptr);
+    x++;
+    printf("%p\n ", ptr);
 }
