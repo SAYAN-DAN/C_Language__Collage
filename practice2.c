7be53d9fcd233f08cc82c27cf76f3ebe3e1a8371
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-11-17 14:21:30 +0530

    Submit

--- practice2.c
+++ practice2.c
@@ -1,35 +1,9 @@
-
-#include <stdio.h>
-
-int main()
-{
-    int n;
-    printf("Enter the number of element ion the array: ");
-    scanf("%d", &n);
-    int arr[n];
-    for (int i = 0; i < n; i++)
+ #include <stdio.h>
+    int main()
     {
-        scanf("%d", &arr[i]);
+        const int ary[4] = {1, 2, 3, 4};
+        int *p;
+        p = ary + 3;
+        *p = 5;
+        printf("%d\n", ary[3]);
     }
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            if (arr[j] > arr[j + 1])
-            {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-    printf("Sorted array is: ");
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-
-    return 0;
-}
