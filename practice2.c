f72055b2774dc622b6e4a4d8a317acaad9617c1c
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-11-22 10:49:02 +0530

    Submit

--- practice2.c
+++ practice2.c
@@ -1,38 +1,8 @@
-
 #include <stdio.h>
-void main()
+int *i;
+int main()
 {
-    int arr[3][3], max;
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            scanf("%d", &arr[i][j]);
-        }
-    }
-
-    printf("The Matrix is\n");
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            printf("%d", arr[i][j]);
-        }
-        printf("\n");
-    }
-
-    max = arr[0][0];
-
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            if (max < arr[i][j])
-            {
-                max = arr[i][j];
-            }
-        }
-    }
-
-    printf("The Maximum number is: %d", max);
+    if (i == 0)
+        printf("true\n");
+    return 0;
 }
