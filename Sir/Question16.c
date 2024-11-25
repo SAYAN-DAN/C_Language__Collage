9b90b65fec9fc631d882b72be4fb906e8a306552
SAYAN-DAN <sayan94.arambagh@gmail.com>
2024-11-25 07:53:48 +0530

    Submit

--- Sir/Question16.c
+++ Sir/Question16.c
@@ -1,17 +1,21 @@
 // Write a program to test weather a number is prime or not...
-#include<stdio.h>
-int main(){
-    int n,i;
+#include <stdio.h>
+int main()
+{
+    int n, i;
     printf("Enter a number: ");
     scanf("%d", &n);
-    for ( i = 2; i <= n-1; i++){
-        if (n % i ==0){
+    for (i = 2; i <= n - 1; i++)
+    {
+        if (n % i == 0)
+        {
             printf("This is not a prime number");
-            break;        
+            break;
         }
     }
-    if ( i == n){
+    if (i == n)
+    {
         printf("This is a prime number");
     }
-    return 0;    
+    return 0;
 }
